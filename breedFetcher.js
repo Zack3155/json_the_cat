da561a0fd2e2f411cbf951822af17f83837f263b
Zack <zhanbin3155@gmail.com>
2021-11-04 17:44:59 -0400

    Testing Cats as a Service

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,21 @@
 const request = require('request');
-const url = 'https://api.thecatapi.com/v1/breeds/search?q=';
-const name = process.argv[2];
 
-let search = url + name;
+const fetchBreedDescription = function(breedName, callback) {
+  const url = 'https://api.thecatapi.com/v1/breeds/search?q=';
+  let search = url + breedName;
 
-request(search, (err, resp, body) => {
-  if (!err) {
-    const data = JSON.parse(body);
-    //console.log(data, data.length);
-    if (data && data.length && resp.statusCode === 200) console.log(data[0].description);
-    else if (resp.statusCode !== 200) console.log('The request failed, plz check ur URL');
-    else console.log('The requested breed is not found');
-  } else console.log('Dear USER: ', err);
-});
+  request(search, (err, resp, body) => {
+    if (!err) {
+      const data = JSON.parse(body);
+      if (data && data.length && resp.statusCode === 200) {
+        callback(null, data[0].description);
+      } else if (resp.statusCode !== 200) callback('The request failed, plz check ur URL', null);
+      else callback('The requested breed is not found', null);
+    } else callback(err, null);
+  });
+
+};
+
+
+
+module.exports = { fetchBreedDescription };
